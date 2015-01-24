d85638ed167cd5e73a4124ea464bebd5d601ba61
Catherine Bui <contactcatherinebui@gmail.com>
2015-01-23 20:53:02 -0800

    Edits readme.md

--- README.md
+++ README.md
@@ -1,7 +1,48 @@
-ObsidianMoose
-=============
-
 Feeder
 
 A simplified food app that helps you find eateries closest to you quickly.
 UI is mobile friendly.
+=======
+A food search app that helps users find food quickly and easily via the Yelp and Google Geocoder APIs.
+
+
+## Team
+
+  - __Product Owner and Front end__: Sarah Wesley
+  - __Scrum Master and Full Stack__: Catherine Bui
+  - __Development Team Members__: Michael Ryan, Jonathan Ng
+
+## Table of Contents
+
+1. [Usage](#Usage)
+1. [Development](#development)
+    1. [Installing Dependencies](#installing-dependencies)
+1. [License](#license)
+
+## Usage
+
+1. Enter in a type of cuisine, specific food item, or a restaurant you have in mind.
+1. A mininum number of stars.
+1. A maximum distance.
+1. And the top three results will be simply delivered, refresh and another three will load!
+
+## Development
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+sudo npm install -g bower
+npm install
+bower install
+```
+
+### Roadmap
+
+View the project roadmap [here](LINK_TO_PROJECT_ISSUES)
+
+
+## License
+
+MIT 2014
--- server/server.js
+++ server/server.js
@@ -2,39 +2,29 @@ var express = require('express');
 var app = express();
 var yelp = require('./yelp');
 var geocoder = require('./geocoder');
-// var passport = require('./auth')
-// var authHelpers = require('./authHelpers');
 var db = require('./databaseHelpers');
 var bodyParser = require('body-parser');
 var util = require('./authHelpers');
 var auth = require('./auth');
 
+
 // serves static pages
-// *********unsure if this will work with angular routing for views, not yet tested with client*******
-app.use(express.static(__dirname + '/../client/'))
-
+app.use(express.static(__dirname + '/../client/'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 
 // post request for food search. sends back top 3 results as JSON
 app.post('/imhungry', function (req, res) {
-  //******* unsure if req.param works currently, might need to use body parse, not yet tested with client **********
   var search = req.param('search');
   var distance = req.param('distance');
   var stars = req.param('stars');
   var location = req.param('loc');
   var lat = location.latitude;
   var lon = location.longitude;
-  // var lon = req.param('loc.longitude');  
-
-  console.log(req.param('search'));
-  console.log(req.param('distance'));
-  console.log(req.param('stars'));
-  console.log(req.param('loc'));
-  // console.log(req.param('loc.longitude'));
 
   //searches yelp, gets businesses and replies with reponse 200 and data
   yelp.searchYelp(search, distance, stars, lat, lon, req, res);
@@ -49,68 +39,62 @@ app.post('/imhungry', function (req, res) {
 // });
 
 
-
-app.post('/login', auth.login);
-app.post('/signup', auth.signup);
-app.get('/signedin', auth.checkAuth);
-
 app.post('/signin', auth.login);
 app.post('/signup', auth.signup);
 app.get('/signedin', auth.checkAuth);
 
 //takes object of form {username: jon, restaurant: yum}
 //adds to likes if not already there
-app.post('/like', function(req, res, next){
-	console.log(req.body);
-	var username = req.body.username || 'kim';
-	console.log(1234, req.body.foodPlace)
-	var restaurant = req.body.foodPlace || 'hi there';
-	console.log(username, restaurant);
-	db.isInLikes(username, restaurant, function(result) {
-		if (!result) {
-			db.addRestaurantToLikes(username, restaurant)
-		} else {
-			console.log('already in likes')
-		}
-	});
+app.post('/like', function (req, res, next) {
+  console.log(req.body);
+  var username = req.body.username || 'kim';
+  console.log(1234, req.body.foodPlace)
+  var restaurant = req.body.foodPlace || 'hi there';
+  console.log(username, restaurant);
+  db.isInLikes(username, restaurant, function (result) {
+    if (!result) {
+      db.addRestaurantToLikes(username, restaurant)
+    } else {
+      console.log('already in likes')
+    }
+  });
 });
 
 
 //takes object of form {username: jon, restaurant: yum}
 //adds to dislikes if not already there
-app.post('/dislike', function(req, res, next){
-	console.log(req.body);
-	var username = req.body.username || 'kim';
-	var restaurant = req.body.restaurant || 'hi';
-	console.log(username, restaurant);
-	db.isInDislikes(username, restaurant, function(result) {
-		if (!result) {
-			db.addRestaurantToDislikes(username, restaurant)
-		} else {
-			console.log('already in dislikes')
-		}
-	});
+app.post('/dislike', function (req, res, next) {
+  console.log(req.body);
+  var username = req.body.username || 'kim';
+  var restaurant = req.body.restaurant || 'hi';
+  console.log(username, restaurant);
+  db.isInDislikes(username, restaurant, function (result) {
+    if (!result) {
+      db.addRestaurantToDislikes(username, restaurant)
+    } else {
+      console.log('already in dislikes')
+    }
+  });
 });
 
-app.post('/likes', function(req, res, next) {
-	console.log('USERNAME', req.body.username);
-	var username = req.body.username || 'kim';
-	db.getLikes(username, function(likes) {
-		console.log('THESE ARE THE LIKES', likes);
-		res.send(likes);
-	});
+app.post('/likes', function (req, res, next) {
+  console.log('USERNAME', req.body.username);
+  var username = req.body.username || 'kim';
+  db.getLikes(username, function (likes) {
+    console.log('THESE ARE THE LIKES', likes);
+    res.send(likes);
+  });
 });
 
-app.get('/dislikes', function(req, res, next) {
-	var username = 'kim';
-	db.getDislikes(username, function(dislikes) {res.send(dislikes)});
+app.get('/dislikes', function (req, res, next) {
+  var username = 'kim';
+  db.getDislikes(username, function (dislikes) {
+    res.send(dislikes)
+  });
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port);
-
-
-
 
 
 
